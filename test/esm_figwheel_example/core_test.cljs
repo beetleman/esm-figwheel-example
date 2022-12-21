526702639ba2730b9d0509537bbc3ad204695598
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2022-12-21 10:54:59 +0100

    init

---
+++ test/esm_figwheel_example/core_test.cljs
@@ -0,0 +1,10 @@
+(ns esm-figwheel-example.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [esm-figwheel-example.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
